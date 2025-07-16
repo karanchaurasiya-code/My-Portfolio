14b71234ea6228645d056c2fca4ff27ab163dd64
Karan <karanchaurasiya2003a@gmail.com>
2025-07-17 02:33:57 +0530

    logo img fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import logo from "../../public/images/logo.png";
 
 const Navbar = ({ menuOpen, setMenuOpen }) => {
   useEffect(() => {
@@ -10,7 +11,7 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
         <div className="max-w-5xl mx-auto px-4">
           <div className="flex justify-between items-center h-16">
             <a href="#" className="font-mono text-xl font-bold text-white">
-              <img src="/images/logo.png" alt="logo-img" className="h-13" />
+              <img src={logo} alt="logo-img" className="h-[52px]" />
             </a>
             <div
               className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
